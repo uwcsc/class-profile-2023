e08988e82c4cdc6c0fb846f78b911c7208b09214
c4thie <64122601+c4thie@users.noreply.github.com>
2024-01-19 18:09:44 -0500

    added 2023 lifestyle-and-interests data pt.2

--- data/lifestyle-and-interests.ts
+++ data/lifestyle-and-interests.ts
@@ -485,278 +485,385 @@ export const L11 = [
   },
 ];
 
-// export const L13 = [
-//   {
-//     category: "Sometimes easy",
-//     value: 5,
-//   },
-//   {
-//     category: "Always easy",
-//     value: 5,
-//   },
-//   {
-//     category: "Always easy and sometimes medium",
-//     value: 25,
-//   },
-//   {
-//     category: "Always easy and medium",
-//     value: 22,
-//   },
-//   {
-//     category: "Always medium and sometimes hard",
-//     value: 32,
-//   },
-//   {
-//     category: "Always medium and hard",
-//     value: 10,
-//   },
-// ];
-
-// export const L14 = [
-//   {
-//     category: "0",
-//     value: 22,
-//   },
-//   {
-//     category: "1",
-//     value: 18,
-//   },
-//   {
-//     category: "2",
-//     value: 22,
-//   },
-//   {
-//     category: "3",
-//     value: 21,
-//   },
-//   {
-//     category: "4",
-//     value: 5,
-//   },
-//   {
-//     category: "5",
-//     value: 8,
-//   },
-//   {
-//     category: "6",
-//     value: 4,
-//   },
-//   {
-//     category: "7",
-//     value: 1,
-//   },
-//   {
-//     category: "8",
-//     value: 1,
-//   },
-// ];
-
-// export const L16 = [
-//   {
-//     category: "Never or rarely",
-//     value: 89.1,
-//   },
-//   {
-//     category: "1+ per year",
-//     value: 9.9,
-//   },
-//   {
-//     category: "Weekly",
-//     value: 1,
-//   },
-// ];
-
 export const L12 = [
   {
-    text: "MathSoc",
-    value: 9,
+    text: "Board Games",
+    value: 4,
   },
   {
-    text: "Blueprint",
-    value: 8,
+    text: "Orientation",
+    value: 4,
   },
   {
-    text: "Muay Thai",
-    value: 8,
+    text: "WISA",
+    value: 1,
+  },
+  {
+    text: "mathNEWS",
+    value: 5,
+  },
+  {
+    text: "Poker",
+    value: 2,
   },
   {
     text: "Volleyball",
-    value: 6,
+    value: 10,
+  },
+  {
+    text: "Breakers",
+    value: 1,
+  },
+  {
+    text: "Human VS Zombies",
+    value: 2,
+  },
+  {
+    text: "None",
+    value: 1,
+  },
+  {
+    text: "Math Ambassadors",
+    value: 1,
   },
   {
     text: "Dodgeball",
-    value: 6,
-  },
-  {
-    text: "Frisbee",
-    value: 7,
-  },
-  {
-    text: "CSC",
-    value: 5,
-  },
-  {
-    text: "Basketball",
-    value: 6,
-  },
-  {
-    text: "UWACC",
-    value: 5,
-  },
-  {
-    text: "Badminton",
-    value: 5,
-  },
-  {
-    text: "Watonomous",
-    value: 4,
-  },
-  {
-    text: "WiCS",
-    value: 4,
-  },
-  {
-    text: "Tech+",
-    value: 4,
-  },
-  {
-    text: "Chess",
-    value: 4,
-  },
-  {
-    text: "PMC",
     value: 3,
   },
   {
-    text: "HipHop",
-    value: 3,
-  },
-  {
-    text: "Orchestra",
-    value: 3,
-  },
-  {
-    text: "Waterloop",
-    value: 3,
-  },
-  {
-    text: "Soccer",
-    value: 3,
-  },
-  {
-    text: "DSC",
-    value: 3,
-  },
-  {
-    text: "Hack the North",
+    text: "Imprint",
     value: 2,
   },
   {
-    text: "ACE",
-    value: 2,
-  },
-  {
-    text: "Cuban Salsa",
-    value: 2,
-  },
-  {
-    text: "Aquadrone",
-    value: 2,
-  },
-  {
-    text: "Magic",
-    value: 2,
-  },
-  {
-    text: "Fencing",
-    value: 2,
+    text: "Midnight Sun Design Team",
+    value: 1,
   },
   {
     text: "CCF",
     value: 2,
   },
   {
-    text: "DJ",
+    text: "Game Dev",
+    value: 1,
+  },
+  {
+    text: "CTRL A",
+    value: 1,
+  },
+  {
+    text: "iGEM",
+    value: 1,
+  },
+  {
+    text: "Dance",
+    value: 1,
+  },
+  {
+    text: "Culture Clash Theatre",
+    value: 1,
+  },
+  {
+    text: "Quidditch",
+    value: 1,
+  },
+  {
+    text: "Softball",
+    value: 1,
+  },
+  {
+    text: "MSA",
+    value: 1,
+  },
+  {
+    text: "Frisbee",
+    value: 12,
+  },
+  {
+    text: "CSC",
+    value: 5,
+  },
+  {
+    text: "DDR",
+    value: 1,
+  },
+  {
+    text: "DDC",
+    value: 1,
+  },
+  {
+    text: "Hockey",
     value: 2,
   },
   {
-    text: "Tennis",
+    text: "Design teams",
     value: 2,
+  },
+  {
+    text: "Squash",
+    value: 1,
+  },
+  {
+    text: "Cheese Club",
+    value: 1,
+  },
+  {
+    text: "Ballroom Dance",
+    value: 2,
+  },
+  {
+    text: "Toastmaster",
+    value: 1,
+  },
+  {
+    text: "ICSN",
+    value: 1,
+  },
+  {
+    text: "Limelight",
+    value: 1,
+  },
+  {
+    text: "Korean Culture",
+    value: 1,
+  },
+  {
+    text: "Waterloop",
+    value: 1,
+  },
+  {
+    text: "FEDS Volunteering Center",
+    value: 1,
+  },
+  {
+    text: "WAT is Zine",
+    value: 1,
+  },
+  {
+    text: "Basketball",
+    value: 9,
+  },
+  {
+    text: "Orchestra",
+    value: 3,
+  },
+  {
+    text: "Student Council",
+    value: 1,
+  },
+  {
+    text: "Futsal",
+    value: 1,
+  },
+  {
+    text: "Fencing",
+    value: 1,
+  },
+  {
+    text: "Fintech",
+    value: 1,
+  },
+  {
+    text: "Blockchain",
+    value: 1,
+  },
+  {
+    text: "ICE",
+    value: 2,
+  },
+  {
+    text: "WATonomous",
+    value: 3,
   },
   {
     text: "UWCSA",
+    value: 1,
+  },
+  {
+    text: "Intramurals",
     value: 2,
   },
   {
-    text: "mathNEWS",
+    text: "Table Tennis",
+    value: 1,
+  },
+  {
+    text: "Quizbowl",
+    value: 1,
+  },
+  {
+    text: "WiCS",
+    value: 4,
+  },
+  {
+    text: "Blueprint",
     value: 2,
   },
   {
-    text: "Toastmasters",
+    text: "Badminton",
+    value: 8,
+  },
+  {
+    text: "Band",
+    value: 1,
+  },
+  {
+    text: "Cafts 4 Charity",
+    value: 1,
+  },
+  {
+    text: "CoffeeNCode",
+    value: 1,
+  },
+  {
+    text: "HipHop",
+    value: 3,
+  },
+  {
+    text: "Muay Thai",
     value: 2,
   },
   {
-    text: "Chamber Ensembles",
+    text: "WatLock",
+    value: 1,
+  },
+  {
+    text: "Soccer",
+    value: 9,
+  },
+  {
+    text: "Animusic",
+    value: 1,
+  },
+  {
+    text: "Taekwondo",
+    value: 1,
+  },
+  {
+    text: "Hack The North",
+    value: 1,
+  },
+  {
+    text: "StarterHacks",
+    value: 1,
+  },
+  {
+    text: "Chamber Choir",
+    value: 1,
+  },
+  {
+    text: "Latin Dance",
+    value: 1,
+  },
+  {
+    text: "DSC",
+    value: 4,
+  },
+  {
+    text: "Osu",
+    value: 1,
+  },
+  {
+    text: "Visual Arts",
+    value: 1,
+  },
+  {
+    text: "Phys",
+    value: 1,
+  },
+  {
+    text: "Smash",
     value: 2,
   },
   {
-    text: "UWFA",
+    text: "AcaBellas",
+    value: 1,
+  },
+  {
+    text: "Cheerleading",
+    value: 1,
+  },
+  {
+    text: "Jam Network",
+    value: 1,
+  },
+  {
+    text: "A Cappella",
+    value: 3,
+  },
+  {
+    text: "Cuban Salsa",
+    value: 1,
+  },
+  {
+    text: "Chabad",
+    value: 1,
+  },
+  {
+    text: "Concrete Design Team",
+    value: 1,
+  },
+  {
+    text: "MATH Residence Tutor",
+    value: 1,
+  },
+  {
+    text: "uWaft",
+    value: 1,
+  },
+  {
+    text: "UWACC",
     value: 2,
   },
   {
-    text: "WiSTEM",
+    text: "Tech+",
+    value: 3,
+  },
+  {
+    text: "MathSoc",
+    value: 7,
+  },
+  {
+    text: "PMAMCO",
+    value: 8,
+  },
+  {
+    text: "Spikeball",
     value: 1,
   },
   {
-    text: "KC",
+    text: "PMC",
     value: 1,
   },
   {
-    text: "WARG",
+    text: "Lacrosse",
     value: 1,
   },
   {
-    text: "Swimming",
+    text: "Recreational Sports",
     value: 1,
   },
   {
-    text: "Skating",
+    text: "UWICA",
     value: 1,
   },
   {
-    text: "Boxing",
+    text: "SERVE",
+    value: 6,
+  },
+  {
+    text: "TCF",
     value: 1,
   },
   {
-    text: "Movie",
+    text: "MLSB",
     value: 1,
   },
   {
-    text: "Theatre",
+    text: "Rocket League",
     value: 1,
   },
   {
-    text: "Cooking",
-    value: 1,
-  },
-  {
-    text: "TechNova",
-    value: 1,
-  },
-  {
-    text: "CTF",
-    value: 1,
-  },
-  {
-    text: "Robotics Design Team",
-    value: 1,
-  },
-  {
-    text: "EntSoc",
-    value: 1,
-  },
-  {
-    text: "UWEN",
+    text: "MEF Board",
     value: 1,
   },
 ];
@@ -764,62 +871,671 @@ export const L12 = [
 export const L13 = [
   {
     category: "MC",
-    value: 23,
-  },
-  {
-    category: "DC",
     value: 22,
   },
   {
+    category: "DC",
+    value: 26,
+  },
+  {
     category: "Res",
-    value: 16,
+    value: 6,
   },
   {
     category: "E7",
-    value: 11,
-  },
-  {
-    category: "QNC",
-    value: 9,
+    value: 13,
   },
   {
     category: "DP",
-    value: 5,
+    value: 10,
   },
   {
     category: "SLC",
-    value: 4,
+    value: 9,
   },
   {
     category: "E5",
-    value: 3,
+    value: 2,
   },
   {
     category: "EV3",
-    value: 3,
+    value: 7,
   },
   {
     category: "PHY",
+    value: 2,
+  },
+  {
+    category: "AHS",
+    value: 1,
+  },
+  {
+    category: "CGR",
+    value: 2,
+  },
+  {
+    category: "STC",
+    value: 2,
+  },
+  {
+    category: "QNC",
+    value: 6,
+  },
+  {
+    category: "EV1",
+    value: 1,
+  },
+  {
+    category: "ML",
+    value: 1,
+  },
+  {
+    category: "M3",
+    value: 1,
+  },
+  {
+    category: "E6",
+    value: 1,
+  },
+  {
+    category: "HH",
+    value: 1,
+  },
+  {
+    category: "NH",
     value: 1,
   },
 ];
 
-export const L14 = [];
-export const L15 = [];
-export const L16 = [];
-export const L17 = [];
-export const L18 = [];
-export const L19 = [];
-export const L20 = [];
-export const L21 = [];
+export const L14 = [
+  {
+    text: "Rocket League",
+    value: 1,
+  },
+  {
+    text: "Gaming",
+    value: 21,
+  },
+  {
+    text: "Board games",
+    value: 8,
+  },
+  {
+    text: "Poker",
+    value: 1,
+  },
+  {
+    text: "Writing",
+    value: 6,
+  },
+  {
+    text: "Piano",
+    value: 7,
+  },
+  {
+    text: "Badminton",
+    value: 14,
+  },
+  {
+    text: "Programming/Coding",
+    value: 5,
+  },
+  {
+    text: "DanceDanceRevolution",
+    value: 1,
+  },
+  {
+    text: "Drawing",
+    value: 8,
+  },
+  {
+    text: "Digital art",
+    value: 2,
+  },
+  {
+    text: "Music",
+    value: 11,
+  },
+  {
+    text: "Gym",
+    value: 9,
+  },
+  {
+    text: "Violin",
+    value: 3,
+  },
+  {
+    text: "Sleep",
+    value: 1,
+  },
+  {
+    text: "Skating",
+    value: 2,
+  },
+  {
+    text: "TV",
+    value: 4,
+  },
+  {
+    text: "Video games",
+    value: 19,
+  },
+  {
+    text: "Reading",
+    value: 19,
+  },
+  {
+    text: "Soccer",
+    value: 7,
+  },
+  {
+    text: "Math",
+    value: 2,
+  },
+  {
+    text: "Knitting",
+    value: 1,
+  },
+  {
+    text: "Climbing",
+    value: 8,
+  },
+  {
+    text: "Volleyball",
+    value: 6,
+  },
+  {
+    text: "Eating",
+    value: 1,
+  },
+  {
+    text: "Watching basketball",
+    value: 1,
+  },
+  {
+    text: "Watching UFC",
+    value: 1,
+  },
+  {
+    text: "Going out",
+    value: 1,
+  },
+  {
+    text: "Ultimate frisbee",
+    value: 4,
+  },
+  {
+    text: "Snowboarding",
+    value: 2,
+  },
+  {
+    text: "Astronomy",
+    value: 1,
+  },
+  {
+    text: "Guitar",
+    value: 6,
+  },
+  {
+    text: "F1",
+    value: 2,
+  },
+  {
+    text: "Watch tennis",
+    value: 1,
+  },
+  {
+    text: "Crochet",
+    value: 1,
+  },
+  {
+    text: "Running",
+    value: 6,
+  },
+  {
+    text: "Art",
+    value: 2,
+  },
+  {
+    text: "Cooking",
+    value: 11,
+  },
+  {
+    text: "Swimming",
+    value: 4,
+  },
+  {
+    text: "Skiing",
+    value: 2,
+  },
+  {
+    text: "Dancing",
+    value: 9,
+  },
+  {
+    text: "Painting",
+    value: 2,
+  },
+  {
+    text: "Skateboarding",
+    value: 1,
+  },
+  {
+    text: "Powerlifting",
+    value: 1,
+  },
+  {
+    text: "Karaoke",
+    value: 1,
+  },
+  {
+    text: "DnD",
+    value: 5,
+  },
+  {
+    text: "YuGiOh",
+    value: 1,
+  },
+  {
+    text: "Getting drunk",
+    value: 1,
+  },
+  {
+    text: "Kpop",
+    value: 3,
+  },
+  {
+    text: "Fashion",
+    value: 1,
+  },
+  {
+    text: "Accordion",
+    value: 1,
+  },
+  {
+    text: "Movie",
+    value: 2,
+  },
+  {
+    text: "Shopping",
+    value: 1,
+  },
+  {
+    text: "Travelling",
+    value: 3,
+  },
+  {
+    text: "Basketball",
+    value: 9,
+  },
+  {
+    text: "League",
+    value: 1,
+  },
+  {
+    text: "Pokemon",
+    value: 1,
+  },
+  {
+    text: "Baking",
+    value: 1,
+  },
+  {
+    text: "Singing",
+    value: 6,
+  },
+  {
+    text: "Biking",
+    value: 1,
+  },
+  {
+    text: "Hockey",
+    value: 2,
+  },
+  {
+    text: "Racing",
+    value: 1,
+  },
+  {
+    text: "Film",
+    value: 1,
+  },
+  {
+    text: "Boxing",
+    value: 1,
+  },
+  {
+    text: "Weightlifting",
+    value: 3,
+  },
+  {
+    text: "Outdoors",
+    value: 1,
+  },
+  {
+    text: "Bassoon",
+    value: 1,
+  },
+  {
+    text: "Music production",
+    value: 2,
+  },
+  {
+    text: "Felting",
+    value: 1,
+  },
+  {
+    text: "Anime",
+    value: 4,
+  },
+  {
+    text: "Bodybuilding",
+    value: 1,
+  },
+  {
+    text: "Walking",
+    value: 4,
+  },
+  {
+    text: "Quidditch",
+    value: 1,
+  },
+  {
+    text: "Crafting",
+    value: 1,
+  },
+  {
+    text: "Design",
+    value: 2,
+  },
+  {
+    text: "Chess",
+    value: 1,
+  },
+  {
+    text: "Politics",
+    value: 2,
+  },
+  {
+    text: "Debate",
+    value: 1,
+  },
+  {
+    text: "Jogging",
+    value: 1,
+  },
+  {
+    text: "Calligraphy",
+    value: 1,
+  },
+  {
+    text: "Stardew Valley",
+    value: 1,
+  },
+  {
+    text: "Sports",
+    value: 1,
+  },
+  {
+    text: "Bass",
+    value: 2,
+  },
+  {
+    text: "Kickboxing",
+    value: 1,
+  },
+  {
+    text: "Photography",
+    value: 2,
+  },
+  {
+    text: "Animating",
+    value: 1,
+  },
+  {
+    text: "Horseback riding",
+    value: 1,
+  },
+  {
+    text: "Table tennis",
+    value: 2,
+  },
+  {
+    text: "Origami",
+    value: 1,
+  },
+  {
+    text: "Teamfight tactics",
+    value: 1,
+  },
+  {
+    text: "Manga",
+    value: 1,
+  },
+  {
+    text: "Rearranging furniture",
+    value: 1,
+  },
+  {
+    text: "Flying planes",
+    value: 1,
+  },
+  {
+    text: "Curling",
+    value: 1,
+  },
+  {
+    text: "None",
+    value: 1,
+  },
+  {
+    text: "Camping",
+    value: 1,
+  },
+  {
+    text: "Physics",
+    value: 1,
+  },
+  {
+    text: "Psychology",
+    value: 1,
+  },
+  {
+    text: "Pooling",
+    value: 1,
+  },
+  {
+    text: "Adrenaline activities",
+    value: 1,
+  },
+];
+export const L15 = [
+  {
+    category: "0-2",
+    value: 91,
+  },
+  {
+    category: "3-4",
+    value: 15,
+  },
+  {
+    category: "5-6",
+    value: 10,
+  },
+  {
+    category: "7-8",
+    value: 2,
+  },
+  {
+    category: "9+",
+    value: 6,
+  },
+];
 
-// export const L19 = [
-//   {
-//     category: "No",
-//     value: 78,
-//   },
-//   {
-//     category: "Yes",
-//     value: 22,
-//   },
-// ];
+export const L16 = [
+  {
+    category: "The Alley",
+    value: 24,
+  },
+  {
+    category: "Gong Cha",
+    value: 10,
+  },
+  {
+    category: "Coco",
+    value: 23,
+  },
+  {
+    category: "Now Tea",
+    value: 16,
+  },
+  {
+    category: "Xing Fu Tang",
+    value: 4,
+  },
+  {
+    category: "Chatime",
+    value: 8,
+  },
+  {
+    category: "Sweet Dreams Teashop",
+    value: 9,
+  },
+  {
+    category: "Onezo Tapioca",
+    value: 1,
+  },
+  {
+    category: "Machi Machi",
+    value: 6,
+  },
+  {
+    category: "O-Hime",
+    value: 1,
+  },
+  {
+    category: "Lait Night",
+    value: 2,
+  },
+  {
+    category: "Tea Talk",
+    value: 1,
+  },
+];
+
+export const L17 = [
+  {
+    category: "Shawarma Plus",
+    value: 38,
+  },
+  {
+    category: "Lazeez",
+    value: 32,
+  },
+  {
+    category: "iPotato",
+    value: 19,
+  },
+  {
+    category: "Osmow's",
+    value: 5,
+  },
+  {
+    category: "Mozy's",
+    value: 6,
+  },
+  {
+    category: "Sultans",
+    value: 1,
+  },
+];
+
+export const L18 = [
+  {
+    category: "Daily",
+    value: 21,
+  },
+  {
+    category: "Few times a week",
+    value: 34,
+  },
+  {
+    category: "Weekly",
+    value: 19,
+  },
+  {
+    category: "Once every few weeks",
+    value: 17,
+  },
+  {
+    category: "Monthly",
+    value: 4,
+  },
+  {
+    category: "Once every few months",
+    value: 7,
+  },
+  {
+    category: "Never",
+    value: 8,
+  },
+];
+
+export const L19 = [
+  {
+    category: "All",
+    value: 6,
+  },
+  {
+    category: "Most",
+    value: 57,
+  },
+  {
+    category: "Half",
+    value: 26,
+  },
+  {
+    category: "Some",
+    value: 37,
+  },
+  {
+    category: "None",
+    value: 3,
+  },
+];
+
+export const L20 = [
+  {
+    category: "Always",
+    value: 26,
+  },
+  {
+    category: "Often",
+    value: 37,
+  },
+  {
+    category: "Sometimes",
+    value: 32,
+  },
+  {
+    category: "Rarely",
+    value: 24,
+  },
+  {
+    category: "Never",
+    value: 9,
+  },
+];
+
+export const L21 = [
+  { text: "O-Week", value: 2 },
+  { text: "Extracurriculars", value: 20 },
+  { text: "Classes", value: 26 },
+  { text: "High school", value: 37 },
+  { text: "Mutual friends", value: 26 },
+  { text: "Residence", value: 8 },
+  { text: "Co-op", value: 7 },
+  { text: "CSC", value: 1 },
+  { text: "Parties", value: 1 },
+  { text: "Everywhere", value: 1 },
+];
