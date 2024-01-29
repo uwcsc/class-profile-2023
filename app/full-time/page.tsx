a3448d0c1871a9af3a98a4fc50f39dc6cc56fb3d
FRANKLIN RAMIREZ <franklin.ramirez6111@gmail.com>
2024-01-28 21:26:32 -0500

    Edited text and descriptions TODO: Data .ts

---
+++ app/full-time/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { BarGraphHorizontal, BarGraphVertical } from "@/components/BarGraph";
+import { BottomNav } from "@/components/BottomNav";
+import { ComponentWrapper } from "@/components/ComponentWrapper";
+import { Header } from "@/components/Header";
+import { PieChart } from "@/components/PieChart";
+import { SectionHeader } from "@/components/SectionHeader";
+import { WordCloud } from "@/components/WordCloud";
+import { P1, P2, P3, P4, P5, P6, P7, P8 } from "@/data/full-time";
+import { pageRoutes } from "@/data/routes";
+import { DefaultProp, barGraphMargin, barGraphProps, barGraphWidth, pieChartProps, wordCloudWidth } from "@/utils/defaultProps";
+import { useWindowDimensions } from "@/utils/getWindowDimensions";
+import { useIsMobile } from "@/utils/isMobile";
+import styles from "../basePage.module.css";
+
+export default function PostGrad() {
+  const pageWidth = useWindowDimensions().width;
+  const isMobile = useIsMobile();
+
+  return (
+    <div className={styles.page}>
+      <Header />
+      <SectionHeader title="Full-time"/>
+
+      <ComponentWrapper
+        heading="What company will you be working for post-grad?"
+        bodyText="Most students have secured full-time positions and will be working full time post-grad. This is more than 50% of respondents. ">
+        <WordCloud
+          data={P1}
+          width={wordCloudWidth(isMobile, pageWidth)}
+          height={DefaultProp.graphHeight}
+          wordPadding={7}
+          desktopMaxFontSize={75}
+          mobileMaxFontSize={48}
+        />
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="Where will you be working from post-grad?"
+        bodyText="Toronto, Ontario, and New York are the two main places most people seem to be moving to start their new life as full-time adults! A majority of students said they will be working in the USA, which is no surprise due to most opportunities in the tech field being in tech hubs like San Francisco, New York, and Seattle. Regardless, it just goes to show that if you work hard, and with just a bit of luck, you can work in your dream location!"
+        align="right"
+        noBackground>
+        <WordCloud
+          data={P1}
+          width={wordCloudWidth(isMobile, pageWidth)}
+          height={DefaultProp.graphHeight}
+          wordPadding={7}
+          desktopMaxFontSize={75}
+          mobileMaxFontSize={48}
+        />
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="How many offers did you decide between? Please enter a number."
+        bodyText="More than half of the respondents chose between 1 or 2 offers!">
+        <div className={styles.graphContainer}>
+          <PieChart data={P3} {...pieChartProps(isMobile, pageWidth)} />
+        </div>
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="Are you returning to a prior co-op?"
+        bodyText="59 out of 81 respondents are returning to a prior co-op on a permanent return offer! This statistic shows that a co-op program lets you test jobs and find what team/company culture fits your liking much earlier in your career.  Additionally, in these co-op positions, students can obtain new connections and leverage them for future full-time employment."
+        align="right"
+        noBackground>
+        <WordCloud
+          data={P4}
+          width={wordCloudWidth(isMobile, pageWidth)}
+          height={DefaultProp.graphHeight}
+          wordPadding={7}
+          desktopMaxFontSize={75}
+          mobileMaxFontSize={48}
+        />
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="If working in Canada, what will be your first-year total compensation (salary + signing + first-year stock + bonus) in CAD?"
+        bodyText="The world of tech offers intriguing annual compensation and salary options.  These compensation packages encompass a mix of components, including salary, signing bonuses, first-year stock options, and more. Many of these salaries start high and will just continue rising as the years go by!">
+        <BarGraphVertical data={P5} {...barGraphProps(isMobile, pageWidth)} />
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="If working outside of Canada, what will be your first-year total compensation (salary + signing + first-year stock + bonus) in CAD?"
+        bodyText="For those working outside of Canada, with a majority of them working in the USA, their first-year total compensation is significantly higher than working in Canada. This disparity highlights the competitive nature of the international tech job market and the opportunities available for individuals willing to explore opportunities abroad."
+        align="right"
+        noBackground>
+        <BarGraphHorizontal data={P6} width={barGraphWidth(isMobile, pageWidth)} height={DefaultProp.graphHeight} margin={barGraphMargin} />
+      </ComponentWrapper>
+
+      <ComponentWrapper
+        heading="What field/career path will you be in post-grad?"
+        bodyText="More than 60% of respondents are set to embark on their careers in software development! However, it is fascinating to see many others venturing into diverse fields such as cloud computing, game development, and various others. Undergraduate is a time for individuals to discover their passions and chart the course for their future careers. "
+        noBackground>
+        <WordCloud
+          data={P8}
+          width={wordCloudWidth(isMobile, pageWidth)}
+          height={DefaultProp.graphHeight}
+          wordPadding={10}
+          desktopMaxFontSize={75}
+          mobileMaxFontSize={48}
+        />
+      </ComponentWrapper>
+
+      <BottomNav leftPage={pageRoutes.intimacyAndDrugs} rightPage={pageRoutes.miscellaneous} />
+    </div>
+  );
+}
---
+++ data/full-time.ts
@@ -0,0 +1,308 @@
+export const P1 = [
+  // G5. What company will you be working for post-grad?
+  {text: 'Citadel', value: 3},
+  {text: 'Unnamed Financial Trading Firm', value: 1},
+  {text: 'Jane Street', value: 4},
+  {text: 'Apple', value: 1},
+  {text: 'Snowflake', value: 2},
+  {text: 'University of Waterloo', value: 1},
+  {text: 'Capital One', value: 2},
+  {text: 'Bloomberg', value: 2},
+  {text: 'Datadog', value: 2},
+  {text: 'Stripe', value: 2},
+  {text: 'Bloomberg or google', value: 1},
+  {text: 'Citadel Securities', value: 1},
+  {text: 'Scale AI', value: 1},
+  {text: 'Rippling', value: 3},
+  {text: 'Microsoft', value: 1},
+  {text: 'Adeptmind', value: 1},
+  {text: 'Roblox', value: 1},
+  {text: 'American Express', value: 1},
+  {text: 'Verkada', value: 2},
+  {text: 'Amazon', value: 3},
+  {text: 'D.E. Shaw', value: 1},
+  {text: 'Uber', value: 3},
+  {text: 'Whatnot', value: 1},
+  {text: 'Google', value: 1},
+  {text: 'Waabi', value: 1},
+  {text: 'Enzuzo', value: 1},
+  {text: 'Jane Street Capital', value: 1},
+  {text: 'Huawei', value: 1},
+  {text: 'Astronomer', value: 1},
+  {text: 'Databricks', value: 3},
+  {text: 'Anyscale', value: 1},
+  {text: 'RideCo', value: 1},
+  {text: 'Relay', value: 1},
+  {text: 'Carbon Neutral Club', value: 1},
+  {text: 'ZeMind Studios', value: 1},
+  {text: 'Nokia', value: 1},
+  {text: 'Meta', value: 1},
+  {text: 'Doordash', value: 1},
+  {text: 'SeatGeek', value: 1},
+  {text: 'Akuna Capital', value: 1},
+  {text: 'Faire', value: 1},
+  {text: 'Sun Life', value: 1},
+  {text: 'Caldera', value: 1},
+  {text: 'Seatgeek', value: 1},
+  {text: 'HRT', value: 1},
+  {text: 'Hudson River Trading', value: 1},
+  {text: 'Opal', value: 1},
+  {text: 'Riot Games', value: 1},
+  {text: 'Intuit', value: 1},
+  {text: 'LinkedIn', value: 1},
+  {text: 'Behaviour Interactive Inc.', value: 1},
+  {text: 'Warp', value: 1}
+]
+;
+
+export const P2 = [
+  // TODO : Combine duplicates
+  // G6. Where will you be working from post grad? 
+  {text: 'New York, US', value: 1},
+  {text: 'New York, NY, US', value: 2},
+  {text: 'Cupertino, California', value: 1},
+  {text: 'Bellevue, Washington, United States', value: 1},
+  {text: 'Waterloo, Ontario, Canada', value: 2},
+  {text: 'Toronto, Ontario, Canada', value: 9},
+  {text: 'New York, New York, United States', value: 4},
+  {text: 'New York City, United States', value: 1},
+  {text: 'New York, New York, USA', value: 8},
+  {text: 'Toronto, Canada', value: 2},
+  {text: 'NYC, New York, USA', value: 1},
+  {text: 'Seattle, Washington, United States', value: 2},
+  {text: 'San Mateo, CA', value: 1},
+  {text: 'San Mateo, California, United States', value: 1},
+  {text: 'NYC, USA', value: 1},
+  {text: 'London, United Kingdom', value: 1},
+  {text: 'San Francisco, CA, US', value: 1},
+  {text: 'San francisco, cali, united states', value: 1},
+  {text: 'Sunnyvale, California, USA', value: 1},
+  {text: 'Toronto, ON, Canada', value: 3},
+  {text: 'New York City, NY, United States of America', value: 1},
+  {text: 'New York City, New York, USA', value: 4},
+  {text: 'San Mateo, CA, USA', value: 1},
+  {text: 'Kitchener, ON, Canada', value: 1},
+  {text: 'New York City, New York, US', value: 1},
+  {text: 'San Francisco, USA', value: 1},
+  {text: 'San Francisco, California, USA', value: 5},
+  {text: 'Markham, Ontario, Canada', value: 1},
+  {text: 'Calgary, AB, Canada', value: 1},
+  {text: 'Waterloo, ON, Canada', value: 1},
+  {text: 'Vancouver, British Columbia, Canada', value: 1},
+  {text: 'Montreal, Quebec', value: 1},
+  {text: 'Ottawa, Ontario, Canada', value: 1},
+  {text: 'San Francisco, CA, USA', value: 1},
+  {text: 'New York, NY, USA', value: 2},
+  {text: 'New York, NY, United States', value: 1},
+  {text: 'New York City, New York, United States', value: 1},
+  {text: 'Chicago, Illinois, USA', value: 1},
+  {text: 'Vancouver, BC, Canada', value: 1},
+  {text: 'New York, New York', value: 2},
+  {text: 'San Francisco, CA', value: 1},
+  {text: 'New York, New York, United States of America', value: 1},
+  {text: 'San Mateo, California, USA', value: 1},
+  {text: 'Bay Area, California, USA', value: 1},
+  {text: 'San Jose, California', value: 1},
+  {text: 'New York, USA', value: 1},
+  {text: 'Los Angeles, CA, USA', value: 1},
+  {text: 'Santa Clara, CA, America', value: 1}
+]
+;
+
+export const P3 = [
+  {
+    // G7. How many offers did you decide between? Please enter a number. 135
+    category: "0",
+    value: 43, // 58
+  },
+  {
+    category: "1",
+    value: 22.2, // 30
+  },
+  {
+    category: "2",
+    value: 17.8, // 24
+  },
+  {
+    category: "3",
+    value: 9.6, // 13
+  },
+  {
+    category: "4",
+    value: 0.7, // 1
+  },
+  {
+    category: "5+",
+    value: 6.7, // 9
+  },
+];
+
+export const P4 = [
+  {
+    // G9. Are you returning to a prior co-op? 81
+    category: "No",
+    value: 1, // 22
+  },
+  {
+    category: "Yes",
+    value: 19, // 59
+  },
+  {
+    category: "2",
+    value: 31, // N/A
+  },
+];
+
+export const P5 = [
+  // G10i. If you are working in Canada, what will be your first year total compensation (salary + signing + first year stock + bonus) in CAD?
+  {
+    // 
+    category: "0",
+    value: 20,
+  },
+  {
+    category: "1",
+    value: 7,
+  },
+  {
+    category: "2",
+    value: 7,
+  },
+  {
+    category: "3",
+    value: 10,
+  },
+  {
+    category: "4",
+    value: 5,
+  },
+  {
+    category: "5",
+    value: 8,
+  },
+  {
+    category: "6",
+    value: 2,
+  },
+  {
+    category: "7",
+    value: 2,
+  },
+  {
+    category: "8",
+    value: 2,
+  },
+  {
+    category: "10",
+    value: 3,
+  },
+  {
+    category: "12",
+    value: 1,
+  },
+  {
+    category: "15",
+    value: 1,
+  },
+  {
+    category: "35",
+    value: 1,
+  },
+];
+
+export const P6 = [
+  {
+    category: "Yes",
+    value: 61.7,
+  },
+  {
+    category: "No",
+    value: 38.3,
+  },
+];
+
+export const P7 = [
+  {
+    category: "51k-100k",
+    value: 2,
+  },
+  {
+    category: "101k-150k",
+    value: 6,
+  },
+  {
+    category: "151k-200k",
+    value: 16,
+  },
+  {
+    category: "201k-250k",
+    value: 18,
+  },
+  {
+    category: "251k-300k",
+    value: 15,
+  },
+  {
+    category: "300k+",
+    value: 21,
+  },
+];
+
+export const P8 = [
+  {
+    text: "Web development",
+    value: 11,
+  },
+  {
+    text: "Finance",
+    value: 8,
+  },
+  {
+    text: "Cloud Computing",
+    value: 6,
+  },
+  {
+    text: "Artificial Intelligence",
+    value: 5,
+  },
+  {
+    text: "Operating System",
+    value: 2,
+  },
+  {
+    text: "Robotics",
+    value: 3,
+  },
+  {
+    text: "Data Science",
+    value: 10,
+  },
+  {
+    text: "Research",
+    value: 6,
+  },
+  {
+    text: "Cybersecurity",
+    value: 2,
+  },
+  {
+    text: "Hardware",
+    value: 4,
+  },
+  {
+    text: "Mobile Development",
+    value: 1,
+  },
+  {
+    text: "Computer Networking",
+    value: 2,
+  },
+  {
+    text: "Programming Languages",
+    value: 5,
+  },
+  {
+    text: "Compilers",
+    value: 1,
+  },
+];
