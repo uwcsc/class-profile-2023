cf084c529551773660dbb3333ba70620aa809aaf
hyper-neutrino <alex.yj.liao@gmail.com>
2024-02-10 00:27:05 -0500

    [CSP-26] Update Post-Grad Page (#25)
    * - Changed post-grad data
    
    * removed unused imports
    
    * fix formatting
    
    ---------
    
    Co-authored-by: FRANKLIN RAMIREZ <franklin.ramirez6111@gmail.com>

--- data/post-grad.ts
+++ data/post-grad.ts
@@ -1,243 +1,88 @@
 export const P1 = [
   {
+    // G1. Do you plan on pursuing post-graduate education?
     category: "No",
-    value: 64.1,
-  },
-  {
-    category: "Not sure",
-    value: 20.4,
-  },
-  {
-    category: "Masters",
-    value: 5,
-  },
-  {
-    category: "PhD",
-    value: 4,
+    value: 50.4, // 67
   },
   {
     category: "PhD + Masters",
-    value: 6.8,
+    value: 3.8, //5
+  },
+  {
+    category: "Not sure",
+    value: 29.3, // 39
+  },
+  {
+    category: "Masters",
+    value: 12, // 16
+  },
+  {
+    category: "PhD",
+    value: 4.5, // 6
   },
 ];
 
 export const P2 = [
   {
-    category: "Yes",
-    value: 78.2,
+    // G2. What proportion of friends will you keep in contact with post-graduation? 131
+    category: "Half",
+    value: 23.7, // 31
   },
   {
-    category: "No",
-    value: 21.8,
+    category: "Some",
+    value: 42, // 55
+  },
+  {
+    category: "Most",
+    value: 32.1, // 42
+  },
+  {
+    category: "All", // 3
+    value: 2.2,
   },
 ];
 
 export const P3 = [
   {
-    text: "Seattle",
-    value: 8,
+    // G3. If you searched for a job, how many onsites/interviews did you get? 135
+    category: "1", // 6
+    value: 4.4,
   },
   {
-    text: "New York",
-    value: 20,
+    category: "2", // 9
+    value: 6.6,
   },
   {
-    text: "Chicago",
-    value: 4,
+    category: "3", // 6
+    value: 4.4,
   },
   {
-    text: "San Francisco",
-    value: 13,
+    category: "4-8", // 21
+    value: 16,
   },
   {
-    text: "California",
-    value: 13,
-  },
-  {
-    text: "GTA / Toronto",
+    category: "9+", // 14
     value: 10,
   },
   {
-    text: "Boston",
-    value: 2,
+    category: "N/A",// 79
+    value: 58.6,
   },
+
 ];
 
 export const P4 = [
   {
-    category: "0",
-    value: 1,
+    // G4. Have you secured a full time position post-grad? 135
+    category: "Yes", // 89
+    value: 66,
   },
   {
-    category: "1",
-    value: 19,
+    category: "No", // 37
+    value: 27.4,
   },
   {
-    category: "2",
-    value: 31,
-  },
-  {
-    category: "3",
-    value: 17,
-  },
-  {
-    category: "4",
-    value: 6,
-  },
-  {
-    category: "5",
-    value: 5,
-  },
-];
-
-export const P5 = [
-  {
-    category: "0",
-    value: 20,
-  },
-  {
-    category: "1",
-    value: 7,
-  },
-  {
-    category: "2",
-    value: 7,
-  },
-  {
-    category: "3",
-    value: 10,
-  },
-  {
-    category: "4",
-    value: 5,
-  },
-  {
-    category: "5",
-    value: 8,
-  },
-  {
-    category: "6",
-    value: 2,
-  },
-  {
-    category: "7",
-    value: 2,
-  },
-  {
-    category: "8",
-    value: 2,
-  },
-  {
-    category: "10",
-    value: 3,
-  },
-  {
-    category: "12",
-    value: 1,
-  },
-  {
-    category: "15",
-    value: 1,
-  },
-  {
-    category: "35",
-    value: 1,
-  },
-];
-
-export const P6 = [
-  {
-    category: "Yes",
-    value: 61.7,
-  },
-  {
-    category: "No",
-    value: 38.3,
-  },
-];
-
-export const P7 = [
-  {
-    category: "51k-100k",
-    value: 2,
-  },
-  {
-    category: "101k-150k",
-    value: 6,
-  },
-  {
-    category: "151k-200k",
-    value: 16,
-  },
-  {
-    category: "201k-250k",
-    value: 18,
-  },
-  {
-    category: "251k-300k",
-    value: 15,
-  },
-  {
-    category: "300k+",
-    value: 21,
-  },
-];
-
-export const P8 = [
-  {
-    text: "Web development",
-    value: 11,
-  },
-  {
-    text: "Finance",
-    value: 8,
-  },
-  {
-    text: "Cloud Computing",
-    value: 6,
-  },
-  {
-    text: "Artificial Intelligence",
-    value: 5,
-  },
-  {
-    text: "Operating System",
-    value: 2,
-  },
-  {
-    text: "Robotics",
-    value: 3,
-  },
-  {
-    text: "Data Science",
-    value: 10,
-  },
-  {
-    text: "Research",
-    value: 6,
-  },
-  {
-    text: "Cybersecurity",
-    value: 2,
-  },
-  {
-    text: "Hardware",
-    value: 4,
-  },
-  {
-    text: "Mobile Development",
-    value: 1,
-  },
-  {
-    text: "Computer Networking",
-    value: 2,
-  },
-  {
-    text: "Programming Languages",
-    value: 5,
-  },
-  {
-    text: "Compilers",
-    value: 1,
+    category: "Neither", // 9
+    value: 6.6,
   },
 ];
