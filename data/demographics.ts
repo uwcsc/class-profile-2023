7383249d9f52b3dce702057e1e4cd7a2aa1667e2
c4thie <64122601+c4thie@users.noreply.github.com>
2024-02-10 00:00:42 -0500

    [CSP-20] Data/Page Update (Demographic) (#19)
    * [CSP-20] Data/Page Update (Demographic)
    
    * fix unused import
    
    * fix label clipping
    
    * squash non-binary categories for pie chart legibility
    
    * remove "0" entry from second languages question
    
    * aggregate high school special programs
    
    * reorder MBTI types to group them in order
    
    ---------
    
    Co-authored-by: hyper-neutrino <alex.yj.liao@gmail.com>

--- data/demographics.ts
+++ data/demographics.ts
@@ -1,60 +1,79 @@
 export const D1 = [
   {
     category: "CS",
-    value: 83.8,
+    value: 89.6,
   },
   {
     category: "CS/BBA",
-    value: 11.4,
+    value: 5.2,
   },
   {
     category: "CFM",
-    value: 4.7,
+    value: 5.2,
   },
 ];
 
 export const D2 = [
   {
     category: "Man",
-    value: 68.6,
+    value: 66.9,
   },
   {
     category: "Woman",
-    value: 27.6,
+    value: 30.9,
   },
   {
     category: "Gender non-conforming",
-    value: 3.8,
+    value: 2.2,
   },
 ];
 
 export const D3 = [
   {
     category: "She/Her/Hers",
-    value: 29.0,
+    value: 30.9,
   },
   {
     category: "He/Him/His",
-    value: 68.2,
+    value: 66.9,
   },
   {
     category: "They/Them/Their",
-    value: 2.8,
+    value: 2.2,
   },
 ];
 
 export const D4 = [
   {
+    category: "2001",
+    value: 7,
+  },
+  {
+    category: "2000",
+    value: 109,
+  },
+  {
+    category: "1999",
+    value: 10,
+  },
+  {
+    category: "1998",
+    value: 1,
+  },
+];
+
+export const D5 = [
+  {
     category: "Black",
-    value: 2,
+    value: 0,
   },
   {
     category: "Latin",
-    value: 1,
+    value: 3,
   },
   {
     category: "East Asian",
-    value: 68,
+    value: 92,
   },
   {
     category: "Middle Eastern",
@@ -62,34 +81,381 @@ export const D4 = [
   },
   {
     category: "South Asian",
-    value: 13,
+    value: 15,
   },
   {
     category: "Southeast Asian",
-    value: 2,
+    value: 8,
   },
   {
     category: "White",
-    value: 18,
+    value: 23,
+  },
+  {
+    category: "Other",
+    value: 2,
+  },
+];
+
+export const D6 = [
+  {
+    category: "No religious affiliation",
+    value: 2,
+  },
+  {
+    category: "Christianity",
+    value: 26,
+  },
+  {
+    category: "Judaism",
+    value: 2,
+  },
+  {
+    category: "Buddhism",
+    value: 3,
+  },
+  {
+    category: "Islam",
+    value: 4,
+  },
+  {
+    category: "Hinduism",
+    value: 5,
+  },
+  {
+    category: "Jainism",
+    value: 1,
+  },
+  {
+    category: "Agnosticism",
+    value: 48,
+  },
+  {
+    category: "Atheism",
+    value: 42,
+  },
+  {
+    category: "Spiritual",
+    value: 2,
+  },
+  {
+    category: "Jewish",
+    value: 1,
+  },
+  {
+    category: "Bahá’í faith",
+    value: 1,
   },
   {
     category: "Prefer not to say",
+    value: 18,
+  },
+];
+
+export const D7 = [
+  {
+    category: "Asexual",
+    value: 3,
+  },
+  {
+    category: "Bisexual",
+    value: 8,
+  },
+  {
+    category: "Gay",
+    value: 4,
+  },
+  {
+    category: "Heterosexual",
+    value: 117,
+  },
+  {
+    category: "Queer",
+    value: 2,
+  },
+  {
+    category: "Pansexual",
+    value: 1,
+  },
+  {
+    category: "Questioning",
+    value: 4,
+  },
+  {
+    category: "Lesbian",
     value: 1,
   },
 ];
 
-export const D5 = [
+export const D8 = [
   {
-    category: "89",
+    category: "1",
+    value: 28,
+  },
+  {
+    category: "1.5",
     value: 1,
   },
   {
-    category: "90",
+    category: "2",
+    value: 88,
+  },
+  {
+    category: "3",
+    value: 13,
+  },
+  {
+    category: "4",
+    value: 1,
+  },
+  {
+    category: "4+",
+    value: 4,
+  },
+];
+
+export const D8i = [
+  {
+    text: "Mandarin",
+    value: 48,
+  },
+  {
+    text: "Chinese",
+    value: 8,
+  },
+  {
+    text: "French",
+    value: 19,
+  },
+  {
+    text: "Albanian",
+    value: 1,
+  },
+  {
+    text: "Cantonese",
+    value: 7,
+  },
+  {
+    text: "English",
+    value: 3,
+  },
+  {
+    text: "Russian",
+    value: 4,
+  },
+  {
+    text: "Tamil",
+    value: 2,
+  },
+  {
+    text: "Hindi",
+    value: 4,
+  },
+  {
+    text: "German",
+    value: 1,
+  },
+  {
+    text: "Spanish",
+    value: 3,
+  },
+  {
+    text: "Ukrainian",
+    value: 1,
+  },
+];
+
+export const D9 = [
+  { category: "Moderate Right", value: 12.3 },
+  { category: "Moderate", value: 27 },
+  { category: "Left", value: 24.6 },
+  { category: "Moderate Left", value: 33.6 },
+  { category: "Right", value: 0.2 },
+];
+
+export const D10 = [
+  {
+    text: "Ontario (Other)",
+    value: 19,
+  },
+  {
+    text: "British Columbia",
+    value: 9,
+  },
+  {
+    text: "Kitchener / Waterloo",
+    value: 1,
+  },
+  {
+    text: "Alberta",
+    value: 5,
+  },
+  {
+    text: "Quebec",
+    value: 1,
+  },
+  {
+    text: "UAE",
+    value: 1,
+  },
+  {
+    text: "Germany",
+    value: 1,
+  },
+  {
+    text: "Manitoba",
+    value: 1,
+  },
+  {
+    text: "Ecuador",
+    value: 1,
+  },
+  {
+    text: "GTA / Toronto",
+    value: 75,
+  },
+  {
+    text: "Vietnam",
+    value: 1,
+  },
+  {
+    text: "Indonesia",
+    value: 1,
+  },
+  {
+    text: "New Brunswick",
+    value: 1,
+  },
+];
+
+export const D11 = [
+  {
+    category: "High School Diploma",
+    value: 5,
+  },
+  {
+    category: "College Diploma",
+    value: 4,
+  },
+  {
+    category: "Bachelor's Degree",
+    value: 44,
+  },
+  {
+    category: "Master's Degree",
+    value: 55,
+  },
+  {
+    category: "Doctoral Degree",
+    value: 22,
+  },
+  {
+    category: "Medical Doctorate",
+    value: 1,
+  },
+  {
+    category: "No diploma",
+    value: 1,
+  },
+];
+
+export const D12 = [
+  {
+    category: "0-50",
+    value: 14,
+  },
+  {
+    category: "51-100",
+    value: 29,
+  },
+  {
+    category: "101-150",
+    value: 31,
+  },
+  {
+    category: "151-200",
+    value: 21,
+  },
+  {
+    category: "201-250",
+    value: 14,
+  },
+  {
+    category: "251-300",
     value: 6,
   },
   {
-    category: "91",
+    category: "301+",
+    value: 11,
+  },
+  {
+    category: "Prefer not to say",
+    value: 9,
+  },
+];
+
+export const D13 = [
+  {
+    category: "0",
+    value: 91,
+  },
+  {
+    category: "1",
+    value: 26,
+  },
+  {
+    category: "2",
+    value: 5,
+  },
+  {
+    category: "3",
+    value: 3,
+  },
+  {
+    category: "4",
+    value: 3,
+  },
+  {
+    category: "5",
     value: 1,
+  },
+];
+
+export const D14 = [
+  {
+    category: "First",
+    value: 53,
+  },
+  {
+    category: "Second",
+    value: 37.9,
+  },
+  {
+    category: "Third and higher",
+    value: 9.1,
+  },
+];
+
+export const D15 = [
+  {
+    category: "87",
+    value: 1,
+  },
+  {
+    category: "88",
+    value: 0,
+  },
+  {
+    category: "89",
+    value: 0,
+  },
+  {
+    category: "90",
+    value: 0,
+  },
+  {
+    category: "91",
+    value: 2,
   },
   {
     category: "92",
@@ -101,244 +467,220 @@ export const D5 = [
   },
   {
     category: "94",
-    value: 10,
-  },
-  {
-    category: "95",
-    value: 15,
-  },
-  {
-    category: "96",
-    value: 22,
-  },
-  {
-    category: "97",
-    value: 21,
-  },
-  {
-    category: "98",
-    value: 10,
-  },
-  {
-    category: "99",
-    value: 8,
-  },
-];
-
-export const D6 = [
-  {
-    category: "Asexual",
-    value: 6,
-  },
-  {
-    category: "Bisexual",
-    value: 12,
-  },
-  {
-    category: "Gay",
-    value: 3,
-  },
-  {
-    category: "Heterosexual",
-    value: 81,
-  },
-  {
-    category: "Queer",
-    value: 3,
-  },
-  {
-    category: "Pansexual",
-    value: 3,
-  },
-  {
-    category: "Questioning",
-    value: 2,
-  },
-  {
-    category: "Prefer not to say",
-    value: 2,
-  },
-];
-
-export const D7 = [
-  {
-    text: "Ontario (Other)",
-    value: 15,
-  },
-  {
-    text: "British Columbia",
-    value: 11,
-  },
-  {
-    text: "Kitchener / Waterloo",
-    value: 5,
-  },
-  {
-    text: "Alberta",
-    value: 3,
-  },
-  {
-    text: "Quebec",
-    value: 3,
-  },
-  {
-    text: "USA",
-    value: 2,
-  },
-  {
-    text: "Hong Kong",
-    value: 2,
-  },
-  {
-    text: "Manitoba",
-    value: 2,
-  },
-  {
-    text: "India",
-    value: 1,
-  },
-  {
-    text: "GTA / Toronto",
-    value: 58,
-  },
-  {
-    text: "United Arab Emirates",
-    value: 1,
-  },
-  {
-    text: "Indonesia",
-    value: 1,
-  },
-  {
-    text: "Saskatchewan",
-    value: 1,
-  },
-];
-
-export const D8 = [
-  {
-    category: "High School Diploma",
-    value: 2,
-  },
-  {
-    category: "College Diploma",
-    value: 6,
-  },
-  {
-    category: "Bachelor's Degree",
-    value: 43,
-  },
-  {
-    category: "Master's Degree",
-    value: 33,
-  },
-  {
-    category: "Doctoral Degree",
-    value: 17,
-  },
-  {
-    category: "Not Applicable",
-    value: 1,
-  },
-  {
-    category: "Prefer not to say",
-    value: 2,
-  },
-];
-
-export const D9 = [
-  {
-    category: "0-50",
-    value: 9,
-  },
-  {
-    category: "51-100",
-    value: 28,
-  },
-  {
-    category: "101-150",
-    value: 22,
-  },
-  {
-    category: "151-200",
-    value: 23,
-  },
-  {
-    category: "201-250",
     value: 7,
   },
   {
-    category: "251-300",
-    value: 4,
-  },
-  {
-    category: "301+",
-    value: 5,
-  },
-  {
-    category: "Prefer not to say",
-    value: 6,
-  },
-];
-
-export const D10 = [
-  {
-    category: "0",
-    value: 74,
-  },
-  {
-    category: "1",
+    category: "95",
     value: 17,
   },
   {
-    category: "2",
-    value: 5,
+    category: "96",
+    value: 28,
   },
   {
-    category: "3",
-    value: 2,
+    category: "97",
+    value: 24,
   },
   {
-    category: "4",
-    value: 4,
+    category: "98",
+    value: 30,
   },
   {
-    category: "5",
-    value: 2,
+    category: "99",
+    value: 9,
+  },
+  {
+    category: "100",
+    value: 1,
   },
 ];
 
-export const D11 = [
+export const D16 = [
   {
-    category: "No religious affiliation",
-    value: 80,
+    category: "No",
+    value: 32.3,
   },
   {
-    category: "Christianity",
-    value: 16,
+    category: "Yes",
+    value: 67.7,
+  },
+];
+
+export const D16i = [
+  {
+    category: "University of Waterloo",
+    value: 70.8,
   },
   {
-    category: "Judaism",
+    category: "University of Toronto",
+    value: 4,
+  },
+  {
+    category: "Western University",
+    value: 2,
+  },
+  {
+    category: "UC Berkeley",
+    value: 2,
+  },
+  {
+    category: "University of Pennsylvania",
+    value: 1,
+  },
+  {
+    category: "University of Michigan",
+    value: 1,
+  },
+  {
+    category: "University of Texas at Austin",
+    value: 1,
+  },
+  {
+    category: "Sheridan College",
+    value: 1,
+  },
+  {
+    category: "McGill University",
+    value: 1,
+  },
+  {
+    category: "Princeton University",
+    value: 1,
+  },
+];
+
+export const D17 = [
+  { category: "None", value: 31 },
+  { category: "AP", value: 45 },
+  { category: "IB", value: 37 },
+  { category: "Gifted", value: 23 },
+  { category: "Language Immersion", value: 8 },
+  { category: "TOPS", value: 5 },
+  { category: "Enriched", value: 5 },
+  { category: "Other", value: 5 },
+  { category: "CEGEP", value: 1 },
+];
+
+export const D18 = [
+  {
+    text: "2000",
+    value: 32,
+  },
+  {
+    text: "2500",
+    value: 2,
+  },
+  {
+    text: "3000",
     value: 3,
   },
   {
-    category: "Buddhism",
-    value: 2,
+    text: "3500",
+    value: 3,
   },
   {
-    category: "Islam",
+    text: "4000",
+    value: 8,
+  },
+  {
+    text: "4500",
     value: 1,
   },
   {
-    category: "Hinduism",
+    text: "4745",
+    value: 1,
+  },
+  {
+    text: "5000",
+    value: 9,
+  },
+  {
+    text: "6000",
+    value: 2,
+  },
+  {
+    text: "7000",
     value: 5,
   },
   {
-    category: "Jainism",
+    text: "8000",
+    value: 5,
+  },
+  {
+    text: "10000",
+    value: 11,
+  },
+  {
+    text: "11000",
+    value: 2,
+  },
+  {
+    text: "11500",
     value: 1,
   },
   {
-    category: "Prefer not to say",
+    text: "12250",
+    value: 1,
+  },
+  {
+    text: "14000",
     value: 2,
   },
+  {
+    text: "15000",
+    value: 3,
+  },
+  {
+    text: "18000",
+    value: 1,
+  },
+  {
+    text: "20000",
+    value: 1,
+  },
+  {
+    text: "25000",
+    value: 1,
+  },
+  {
+    text: "30000",
+    value: 1,
+  },
+  {
+    text: "35000",
+    value: 1,
+  },
+  {
+    text: "40000",
+    value: 1,
+  },
+  {
+    text: "41234",
+    value: 1,
+  },
+  {
+    text: "93000",
+    value: 1,
+  },
+  {
+    text: "N/A",
+    value: 36,
+  },
+];
+
+export const D19 = [
+  { category: "ISTJ", value: 10 },
+  { category: "ISFJ", value: 5 },
+  { category: "INFJ", value: 8 },
+  { category: "INTJ", value: 13 },
+  { category: "ISTP", value: 7 },
+  { category: "ISFP", value: 1 },
+  { category: "INFP", value: 10 },
+  { category: "INTP", value: 18 },
+  { category: "ESTP", value: 4 },
+  { category: "ENFP", value: 7 },
+  { category: "ENTP", value: 11 },
+  { category: "ESFJ", value: 3 },
+  { category: "ENFJ", value: 9 },
+  { category: "ENTJ", value: 6 },
 ];
