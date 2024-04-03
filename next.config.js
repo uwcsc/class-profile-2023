8568484d9321959f0d1a3277207d4ccbc0f4ba69
hyper-neutrino <alex.yj.liao@gmail.com>
2024-04-03 11:43:54 -0400

    change build to static

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath,
   assetPrefix: `${basePath}/`,
   publicRuntimeConfig: { basePath },
