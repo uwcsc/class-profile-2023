71f8e56c3c4252f63567430684ba396109432ec6
hyper-neutrino <alex.yj.liao@gmail.com>
2024-04-04 02:22:34 -0400

    add discord embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import { Viewport } from "next";
+import { title } from "../utils/title";
 import "./globals.css";
+
+export const metadata = title("Home");
+
+export const viewport: Viewport = {
+  colorScheme: "dark",
+  themeColor: "#5caff9",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
